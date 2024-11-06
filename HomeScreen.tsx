aebb3cdb3a2d4f9f3032fa913c1e23d109737e8b
Soqlo1 <vladyslavmotylev@gmail.com>
2024-11-06 02:37:24 +0200

    Update HomeScreen.tsx

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native-web';
+import { View, StyleSheet } from 'react-native';
 import Calendar from './Calendar';
 import { CycleDay } from './CycleTypes';
 
